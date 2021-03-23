2365dadd54343565ac0ba345174a5e3a7b0b74f3
luiizsilverio <luiiz.silverio@gmail.com>
2021-03-23 08:59:38 -0300

    baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   //baseURL: 'http://localhost:3000/api',
-  baseURL: 'https://dtmoney-react.vercel.app:3000/api',
+  baseURL: 'https://dtmoney-react.vercel.app/api',
 })
