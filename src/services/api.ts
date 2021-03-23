10d79ea67caac5807b939bf3235f1db19ce7c7a5
luiizsilverio <luiiz.silverio@gmail.com>
2021-03-23 09:13:37 -0300

    baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,3 +4,10 @@ export const api = axios.create({
   //baseURL: 'http://localhost:3000/api',
   baseURL: 'https://dtmoney-react.vercel.app/api',
 })
+
+
+/*
+Utiliza a fake API MirageJs, por isso não precisa rodar o servidor em paralelo.
+Para rodar localmente, use localhost
+Para rodar na Vercel, use a segunda URL
+*/
